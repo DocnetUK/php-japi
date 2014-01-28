26d354dbbc9fd8d66f6f677397d8dd2391555497
twalder-docnet <tom@doc-net.com>
2014-01-28 01:23:49 +0000

    First commit of Router tests

--- tests/router.php
+++ tests/router.php
@@ -21,4 +21,77 @@
  * @author Tom Walder <tom@docnet.nu>
  */
 
-$obj_router = new \Docnet\JAPI\Router();
+date_default_timezone_set("Europe/London");
+require('BaseTest.php');
+require('../src/Docnet/JAPI/Config.php');
+require('../src/Docnet/JAPI.php');
+require('../src/Docnet/JAPI/Exceptions/Routing.php');
+require('../src/Docnet/JAPI/Interfaces/Router.php');
+require('../src/Docnet/JAPI/Router.php');
+
+
+/**
+ * RouterTests
+ *
+ * @author Tom Walder <tom@docnet.nu>
+ */
+class RouterTests extends BaseTest
+{
+
+    public function testHelloWorld()
+    {
+        try {
+            $obj_router = new \Docnet\JAPI\Router();
+            $obj_router->route('/hello/world');
+        } catch (\Exception $obj_ex) {}
+        $this->assertIsArray($obj_router->getRouting(), array('\Hello', 'worldAction'));
+    }
+
+    public function testMultiPart()
+    {
+        try {
+            $obj_router = new \Docnet\JAPI\Router();
+            $obj_router->route('/hello-world/long-name');
+        } catch (\Exception $obj_ex) {}
+        $this->assertIsArray($obj_router->getRouting(), array('\HelloWorld', 'longNameAction'));
+    }
+
+    public function testCasing()
+    {
+        try {
+            $obj_router = new \Docnet\JAPI\Router();
+            $obj_router->route('/heLLo-wORld/long-NaMe');
+        } catch (\Exception $obj_ex) {}
+        $this->assertIsArray($obj_router->getRouting(), array('\HelloWorld', 'longNameAction'));
+    }
+
+    protected function testStaticRoute()
+    {
+        try {
+            $obj_router = new \Docnet\JAPI\Router();
+            $obj_router->addRoute('/goodbye', 'Testing', 'StaticRoutes');
+            $obj_router->route('/goodbye');
+        } catch (\Exception $obj_ex) {}
+        $this->assertIsArray($obj_router->getRouting(), array('Testing', 'StaticRoutes'));
+    }
+
+    protected function testStaticRoutes()
+    {
+        try {
+            $obj_router = new \Docnet\JAPI\Router();
+            $obj_router->addRoute('/goodbye', 'Testing', 'StaticRoutes');
+            $obj_router->route('/goodbye');
+        } catch (\Exception $obj_ex) {}
+        $this->assertIsArray($obj_router->getRouting(), array('Testing', 'StaticRoutes'));
+
+        try {
+            $obj_router->addRoute('/goodbye/cruel/world', 'TestingAgain', 'MoreStaticRoutes');
+            $obj_router->route('/goodbye/cruel/world');
+        } catch (\Exception $obj_ex) {}
+        $this->assertIsArray($obj_router->getRouting(), array('TestingAgain', 'MoreStaticRoutes'));
+    }
+
+}
+
+$obj_tests = new RouterTests();
+$obj_tests->run();
