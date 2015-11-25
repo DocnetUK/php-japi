9c6ca69caa26d8eabd5e7eddce553e873f0ca50e
twalder-docnet <tom@doc-net.com>
2015-11-25 09:25:02 +0000

    Change error handling behaviour

--- src/Docnet/JAPI.php
+++ src/Docnet/JAPI.php
@@ -19,6 +19,7 @@ namespace Docnet;
 use Docnet\JAPI\Controller;
 use Docnet\JAPI\Exceptions\Routing as RoutingException;
 use Docnet\JAPI\Exceptions\Auth as AuthException;
+use Guzzle\Common\Exception\RuntimeException;
 use Psr\Log\LoggerAwareInterface;
 
 /**
@@ -41,6 +42,7 @@ class JAPI implements LoggerAwareInterface
     public function __construct()
     {
         register_shutdown_function([$this, 'timeToDie']);
+        set_error_handler([$this, 'errorHandler']);
     }
 
     /**
@@ -81,38 +83,62 @@ class JAPI implements LoggerAwareInterface
 
     /**
      * Custom shutdown function
+     *
+     * From PHP manual:
+     * The following error types cannot be handled with a user defined function:
+     * E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR
+     * E_CORE_WARNING, E_COMPILE_WARNING
      */
     public function timeToDie()
     {
         $arr_error = error_get_last();
-        if ($arr_error && in_array($arr_error['type'], [E_ERROR, E_USER_ERROR, E_COMPILE_ERROR])) {
+        if ($arr_error && in_array($arr_error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
             $this->jsonError(new \ErrorException($arr_error['message'], 500, 0, $arr_error['file'], $arr_error['line']), 500);
         }
     }
 
     /**
-     * Whatever went wrong, let 'em have it in JSON over HTTP
+     * This function is to complete the error-handling picture by dealing with error types that can be dealt with by
+     * user functions. It elevates them to Exceptions.
      *
-     * @todo Environment or LIVE check for extended error
+     * Return true to stop other error handlers executing.
+     *
+     * @param $int_err
+     * @param $str_err
+     * @param $str_file
+     * @param $int_line
+     * @return bool
+     * @throws \ErrorException
+     */
+    public function errorHandler($int_err, $str_err, $str_file, $int_line)
+    {
+        if(in_array($int_err, [E_RECOVERABLE_ERROR, E_USER_ERROR])) {
+            throw new \ErrorException($str_err, $int_err, 0, $str_file, $int_line);
+        }
+        return true;
+    }
+
+    /**
+     * Whatever went wrong, let 'em have it in JSON over HTTP
      *
      * @param \Exception $obj_error
      * @param int $int_code
      */
     protected function jsonError(\Exception $obj_error, $int_code)
     {
+        $bol_internal = ($obj_error instanceof \ErrorException);
+        $str_log_detail = $obj_error->getMessage();
+        $str_exception = get_class($obj_error);
         $arr_response = [
             'code' => $int_code,
-            'msg' => ($obj_error instanceof \ErrorException ? 'Internal Error' : 'Exception')
+            'msg' => ($bol_internal ? 'Internal Error' : $str_log_detail)
         ];
-        $str_log_message = $obj_error->getMessage();
-        if(TRUE) { // @todo Environment or LIVE check
-            $arr_response['detail'] = $str_log_message;
-        }
+        // Adjust the code for HTTP response
         if($int_code < 400 || $int_code > 505) {
             $int_code = 500;
         }
         $this->sendResponse($arr_response, $int_code);
-        $this->getLogger()->error("[JAPI] [{$int_code}] Error: {$str_log_message}");
+        $this->getLogger()->error("[JAPI] [{$int_code}] [{$str_exception}] {$str_log_detail}");
     }
 
     /**
