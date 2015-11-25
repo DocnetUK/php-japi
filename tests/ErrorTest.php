ff170058069ecf3be347a06c8ca5911d99d998e9
twalder-docnet <tom@doc-net.com>
2015-11-25 10:21:00 +0000

    Add more error tests

--- tests/ErrorTest.php
+++ tests/ErrorTest.php
@@ -39,4 +39,39 @@ class ErrorTest extends PHPUnit_Framework_TestCase
         $obj_japi->bootstrap(new Exceptional());
     }
 
+    private function mockJapiForJsonError($arr_resp, $int_code)
+    {
+        // Mock JAPI
+        $obj_japi = $this->getMockBuilder('\\Docnet\\JAPI')->setMethods(['sendResponse'])->getMock();
+        $obj_japi->expects($this->once())->method('sendResponse')->with(
+            $this->equalTo($arr_resp),
+            $this->equalTo($int_code)
+        );
+        return $obj_japi;
+    }
+
+    /**
+     * Test E_USER_ERROR
+     */
+    public function testUserError()
+    {
+        $obj_japi = $this->mockJapiForJsonError(['code' => E_USER_ERROR, 'msg' => 'Internal Error'], 500);
+        $obj_japi->bootstrap(function(){
+            trigger_error('Test E_USER_ERROR', E_USER_ERROR);
+        });
+    }
+
+    /**
+     * Test invalid code
+     */
+    public function testNonObjectError()
+    {
+        $obj_japi = $this->mockJapiForJsonError(['code' => E_RECOVERABLE_ERROR, 'msg' => 'Internal Error'], 500);
+        $obj_japi->bootstrap(function(){
+            // Nonsense code (on purpose)
+            $this->$this->wer();
+        });
+    }
+
+
 }
