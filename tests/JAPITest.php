8aa17e1eba7374d4db8d5b10bfdc500ae68b37b4
twalder-docnet <tom@doc-net.com>
2015-11-25 09:25:26 +0000

    Set up new test for error handler testing

--- tests/JAPITest.php
+++ tests/JAPITest.php
@@ -54,40 +54,6 @@ class JAPITest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test Exceptions are correctly passed to jsonError from the bootstrap() method
-     */
-    public function testBootstrapErrorCycle()
-    {
-        // Mock JAPI
-        $obj_japi = $this->getMockBuilder('\\Docnet\\JAPI')->setMethods(['sendResponse', 'jsonError'])->getMock();
-        $obj_japi->expects($this->never())->method('sendResponse');
-        $obj_japi->expects($this->once())->method('jsonError')->with(
-            $this->equalTo(new Exception()),
-            $this->equalTo(0)
-        );
-
-        // Dispatch
-        $obj_japi->bootstrap(new Whoops());
-    }
-
-    /**
-     * Test custom Exception codes are correctly passed to jsonError from the bootstrap() method
-     */
-    public function testBootstrapCustomErrorCycle()
-    {
-        // Mock JAPI
-        $obj_japi = $this->getMockBuilder('\\Docnet\\JAPI')->setMethods(['sendResponse', 'jsonError'])->getMock();
-        $obj_japi->expects($this->never())->method('sendResponse');
-        $obj_japi->expects($this->once())->method('jsonError')->with(
-            $this->equalTo(new RuntimeException('Error Message', 400)),
-            $this->equalTo(400)
-        );
-
-        // Dispatch
-        $obj_japi->bootstrap(new Exceptional());
-    }
-
-    /**
      * Validate the response data from the Controller is correctly passed to sendResponse()
      */
     public function testSendResponse()
